997747ae56e3cd4d3977d95ee02642479bd99120
julia774 <julia.gaska@lo1.sandomierz.pl>
2019-12-18 08:46:00 +0100

    zniama

---
+++ prostokat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+
+
+def main(args):
+    a = int(input("podaj szerokość:")) 
+    b = int(input("podaj wysokość:")) 
+    c = int(input("podaj znak, którym bedzie drukowany prostokąt:")) 
+    print("prostokat" )
+        
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

e61caf1e5930bdba5883e28c414fe0eb8eb78c55
julia774 <julia.gaska@lo1.sandomierz.pl>
2020-01-08 08:40:18 +0100

    dodanie kodu pozwalającego zliczyć średnią

---
+++ Python/oceny.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  oceny.py
+
+def pobierzOceny():
+    oceny = []
+    ocena = float(input("podaj ocenę:"))
+    while ocena > 0:
+        if ocena < 7:
+            oceny.append(ocena)
+        else:
+            print("Błędna ocena!")
+        ocena = float(input("podaj ocenę:"))
+    return oceny
+
+def liczSrednia(oceny):
+    print(sum(oceny) / len(oceny)) 
+    
+
+
+def main(args):
+    oceny = pobierzOceny()
+    liczSrednia(oceny)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
